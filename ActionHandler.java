624dfb6126898ae7d65a731e76a7fef773824c01
AbdoMan19 <102483897+AbdoMan19@users.noreply.github.com>
2022-06-26 03:21:54 +0200

    Add files via upload

---
+++ ActionHandler.java
@@ -0,0 +1,18 @@
+
+package sig.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author abdel
+ */
+public class ActionHandler implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Action Handling called");
+    }
+    
+}
